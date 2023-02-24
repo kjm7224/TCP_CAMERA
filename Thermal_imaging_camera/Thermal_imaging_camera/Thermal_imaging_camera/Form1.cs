971d6d52896750768786106ba9a7852bec7404e8
Kimjongmoon <kjm7224@naver.com>
2023-02-24 11:27:18 +0900

    송신 및 수신 완료 (초기화시 쓰레드 관련된 문제 발생)

---
+++ Thermal_imaging_camera/Thermal_imaging_camera/Thermal_imaging_camera/Form1.cs
@@ -0,0 +1,117 @@
+﻿using header;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+#region ModbusRTU
+using Modbus.Device;
+using System.IO.Ports;
+using System.Threading;
+using System.Diagnostics;
+using Microsoft.SqlServer.Server;
+#endregion
+
+
+namespace Thermal_imaging_camera
+{
+    public partial class Form1 : Form
+    {
+        
+        Header DeviceSetting = new Header();
+        public bool blsTemp;
+        public int nTemp;
+        public const int BAUDRATE = 9600;
+        private const int INITIALIZEPORT = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            DeviceSetting.blsbWorking = false;
+         }
+        
+
+        #region 시작버튼 클릭시
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            blsFlag1 = false;
+            DeviceSetting.PortNum = INITIALIZEPORT;
+            DeviceSetting.strPortName = "COM"+ DeviceSetting.PortNum.ToString();
+            while (tbDebug.Text!="")
+            {
+                Monitor.Enter(tbDebug);
+                tbDebug.Text = "";
+                Monitor.Exit(tbDebug);
+            }
+            MessageBox.Show("초기화 완료");
+            button2.Enabled = !blsFlag1;
+        }
+        #endregion
+
+        bool blsFlag1=false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            blsFlag1 = !blsFlag1;
+            //DeviceSetting.Thread_Start();
+            button2.Enabled = !blsFlag1;
+
+            Thread DisplayThread = new Thread(bWorking);
+            DisplayThread.Start();
+        }
+
+
+        /// <summary>
+        /// 텍스트 박스에 이미지 뿌리기 위한 멀티 쓰레드 준비하기 
+        /// </summary>
+        SerialPort port = new SerialPort();
+        private void bWorking() 
+        {
+            DeviceSetting.PortNum = DeviceSetting.FindCom(DeviceSetting.strPortName, DeviceSetting.PortNum);
+            DeviceSetting.strPortName = "COM" + DeviceSetting.PortNum.ToString();
+            port.PortName = DeviceSetting.strPortName;
+            port.BaudRate = BAUDRATE;
+            Thread.Sleep(100);
+                     
+            port.Open();
+            while (blsFlag1) 
+            {
+                int nReceiveData = port.ReadByte();
+                if (this.InvokeRequired ) 
+                {
+                    this.Invoke(new MethodInvoker(delegate () 
+                    {
+                        tbDebug.Text += string.Format("{0:X2}", nReceiveData);
+                    }));
+                }
+                
+                
+                
+                Thread.Sleep(10);
+                
+            }
+            port.Close();
+            return;
+        }
+      
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Monitor.Enter(this);
+            try
+            {
+                port.Write(tbDebug_Send.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Auto Connect를 먼저 클릭하세요.");
+                
+            }
+            
+            Monitor.Exit(this);
+        }
+    }
+}
---
+++ Thermal_imaging_camera/Thermal_imaging_camera/Thermal_imaging_camera/Header.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thermal_imaging_camera;
+#region ModbusRTU
+using Modbus.Device;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Threading;
+using Thermal_imaging_camera;
+using System.Data;
+#endregion
+
+namespace header
+{
+    internal class Header
+    {
+        
+        
+        
+        
+        
+        public string strPortName;
+        public bool blsbWorking;
+        public int PortNum=1;
+        private const int READTIMEOUT    =            100;
+        private const int WRITETIMEOUT   =            100;
+        public const int BAUDRATE        =            9600;
+        
+        Thread Debug_Thread = new Thread(Func);
+        private static string strCpyPortName;
+
+        //Port 객체 생성
+        SerialPort _port;
+        ModbusSerialMaster _master;
+
+        public void PortOpen(string portName) 
+        {
+           
+
+            //Setting Port
+            _port = new SerialPort(portName,BAUDRATE);
+            _port.ReadTimeout = READTIMEOUT;
+            _port.WriteTimeout = WRITETIMEOUT;
+            _port.Open();
+            strCpyPortName = portName;
+           
+        }
+        public int FindCom(string strPortName,int PortNum) 
+        {
+            try
+            {
+                PortOpen(strPortName);
+                MessageBox.Show("포트 이름 : "+ strPortName);
+                _port.Close();
+            }
+            catch (Exception)
+            {
+                string strTempPortName;
+                PortNum++;
+                strTempPortName = "COM"+PortNum.ToString();
+                PortNum = FindCom(strTempPortName, PortNum);
+                if (PortNum == 20) 
+                {
+                    MessageBox.Show(strTempPortName);
+                    return 0;
+                }
+            }
+            return PortNum;
+        }
+
+        public void PortClose() 
+        { 
+            _port.Close();
+        }
+        
+        private static void Func() 
+        {
+            SerialPort port = new SerialPort(strCpyPortName, BAUDRATE);
+            Form1 form1 = new Form1();
+            
+            //port.ReadTimeout = READTIMEOUT;
+            //port.WriteTimeout = WRITETIMEOUT;
+            port.Open();
+            while (true)//후에 루프문 끊는것 생각해보기 
+            {
+                Monitor.Enter(port);
+                Thread.Sleep(10);
+                int nReceiveData = port.ReadByte();
+                form1.nTemp = nReceiveData;
+                Monitor.Exit(port);
+            }
+            
+        }
+        public bool Return_blsWorking(bool blsWorking) 
+        {
+            return blsWorking;
+        }
+        public void Thread_Start() 
+        {
+            Debug_Thread.Start();
+        }
+        
+    }
+}
